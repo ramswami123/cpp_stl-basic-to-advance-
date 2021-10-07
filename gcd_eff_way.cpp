1003195614d691e06548b872be01e7e5e854ba85
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-10-07 19:39:06 +0530

    Update gcd_eff_way.cpp

--- gcd_eff_way.cpp
+++ gcd_eff_way.cpp
@@ -2,20 +2,15 @@
 using namespace std;
 //optmiszed impelementation of gcd programme
 int gcd(int a,int b){
-   if(a>b) swap(a,b);
-   
    if(b==0){
        return a;
     }
     else{
-        return gcd(a,b%a);
+        return gcd(b,b%a);
     }
 }
 int main(){
    int a,b;
    cin>> a>> b;
-   //condition a is min ,bis max
-   a=min(a,b);
-   b=max(b,a);
    gcd(a,b);
 }
